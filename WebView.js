81d433ec340723643c9939802beb2c8a13b954c7
Sergiy Yemelyanov <sergey.yemelyanov@swanlogic.com>
2020-06-16 14:17:25 +0300

    Fix webview

--- WebView.js
+++ WebView.js
@@ -1,6 +1,6 @@
 import EventEmitter from 'events';
 import React, { Component } from 'react';
-import { WebView as NativeWebView } from 'react-native';
+import { WebView as NativeWebView } from '../react-native-webview';
 import { RN_MESSAGES_CHANNEL_PREFIX } from './config';
 
 export class WebView extends Component {
@@ -29,8 +29,8 @@ export class WebView extends Component {
     if (data.indexOf(RN_MESSAGES_CHANNEL_PREFIX) !== 0) {
       return; // that's not something that was received from rn messages channel
     }
-    
-    // remove the unique identifier so that only the user's original message 
+
+    // remove the unique identifier so that only the user's original message
     // remains
     const jsonString = data.replace(RN_MESSAGES_CHANNEL_PREFIX, '');
 
